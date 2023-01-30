f4dbb37c4c9354e7669b9a6557a96eb448714c1d
Muhab Abdelreheem <mabdulrahim2@outlook.sa>
2023-01-30 07:43:55 +0300

    Update to visual studio realtime priority script - run every 11 seconds
    Run script every 11 seconds.

--- Visual studio realtime priority.bat
+++ Visual studio realtime priority.bat
@@ -1,6 +1,9 @@
+:loop
 @ECHO OFF
 ECHO Setting process priority    
-wmic process where name="devenv.exe" CALL setpriority "High priority"
+wmic process where name="conhost.exe" CALL setpriority "Realtime"
+wmic process where name="cmd.exe" CALL setpriority "Realtime"
+wmic process where name="devenv.exe" CALL setpriority "Above normal"
 wmic process where name="Microsoft.ServiceHub.Controller.exe" CALL setpriority "Realtime"
 wmic process where name="ServiceHub.VSDetouredHost.exe" CALL setpriority "Realtime"
 wmic process where name="ServiceHub.SettingsHost.exe" CALL setpriority "Realtime"
@@ -15,11 +18,11 @@ wmic process where name="ServiceHub.IdentityHost.exe" CALL setpriority "Realtime
 wmic process where name="ServiceHub.IndexingService.exe" CALL setpriority "Realtime"
 wmic process where name="PerfWatson2.exe" CALL setpriority "Realtime"
 wmic process where name="MSBuild.exe" CALL setpriority "Realtime"
-wmic process where name="conhost.exe" CALL setpriority "Realtime"
 wmic process where name="VBCSCompiler.exe" CALL setpriority "Realtime"
 wmic process where name="adb.exe" CALL setpriority "Realtime"
 wmic process where name="git.exe" CALL setpriority "Realtime"
 wmic process where name="javac.exe" CALL setpriority "Realtime"
 wmic process where name="java.exe" CALL setpriority "Realtime"
 wmic process where name="aapt2.exe" CALL setpriority "Realtime"
-wmic process where name="svchost.exe" CALL setpriority "Realtime"
+timeout /t 11
+goto loop
